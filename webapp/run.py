f770fd06152e8cadf608a890d91956d6e7be4257
yx887 <ryjxiao@nyu.edu>
2014-12-07 00:27:18 -0500

    player list completed

--- webapp/run.py
+++ webapp/run.py
@@ -24,11 +24,15 @@ def render_leaders(stats, n_top, year, temp_dir, temp_file, position):
     
     return template.render(data=leader_stats, years=YEARS, year=year, season='{}-{}'.format(int(year)-1, year), position=POSITIONS[position], plots=plots)
 
-def render_players(option, temp_dir, temp_file):
+def render_players(players, position, option, temp_dir, temp_file):
+    """ """
+    players_in_pos = np.array(players.PLAYER[players['POS']==position.upper()])
+    n = len(players_in_pos)
+    players_cols = [players_in_pos[:n/4+1], players_in_pos[(n/4+1):(n/2+2)], players_in_pos[(n/2+2):(3*n/4+3)], players_in_pos[(3*n/4+3):]]
+    count = np.array(players.groupby('POS').count()['PLAYER'][['C', 'PF', 'SF', 'SG', 'PG']])
     env = Environment(loader=PackageLoader(PKG, temp_dir))
     template = env.get_template(temp_file)
-    header_dict = {'info':'League Info', 'current':'Current Players', 'historic':'Historic Players'}
-    return template.render(header=header_dict[option])
+    return template.render(option=option, position=position, count=count, players=players_cols)
 
 class OverviewHandler(tornado.web.RequestHandler):
     """ Request Handler for /overview/year """
@@ -38,7 +42,7 @@ class OverviewHandler(tornado.web.RequestHandler):
             stats = pd.read_csv(filename)
         except IOError:
             print 'data not found'
-            raise tornado.web.HTTPError(404)
+            raise tornado.web.HTTPError(500)
 
         self.write(render_leaders(stats, 12, year, TEMPLATE_DIR, 'overview.html', 'all'))
 
@@ -49,15 +53,25 @@ class PositionHandler(tornado.web.RequestHandler):
             stats = pd.read_csv('nbastats/static/data/stats_{}.csv'.format(year))
         except IOError:
             print 'data not found'
-            raise tornado.web.HTTPError(404)
+            raise tornado.web.HTTPError(500)
         pos = position.upper()
         self.write(render_leaders(stats[stats.POS==pos], 8, year, TEMPLATE_DIR, 'overview.html', position))
 
-class PlayersHandler(tornado.web.RequestHandler):
+class PlayersListHandler(tornado.web.RequestHandler):
     """ Request handler for /players/name """
-    def get(self, option):
-        self.write(render_players(option, TEMPLATE_DIR, 'players_index.html'))
-
+    def get(self, option, position):
+        try:
+            players = pd.read_csv('nbastats/static/data/players_{}.csv'.format(option))
+        except IOError:
+            raise tornado.web.HTTPError(500)
+        
+        self.write(render_players(players, position, option, TEMPLATE_DIR, 'players_index.html'))
+        
+class PlayersOverviewHandler(tornado.web.RequestHandler):
+    """ """
+    def get(self):
+        pass
+    
 def make_app():
     settings = {"debug": True,
                 "static_path": os.path.join(os.path.dirname(__file__), "nbastats/static"),}
@@ -66,8 +80,11 @@ def make_app():
         (r'/static/(.*)', tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
         (r'/overview/(20[01][0-9])', OverviewHandler),
         (r'/overview/(20[01][0-9])/(c|pf|sf|sg|pg)', PositionHandler),
-        (r'/players/(info|current|historic)', PlayersHandler),
-        (r'/.*', tornado.web.RedirectHandler, {'url': '/overview/2014'}),
+        (r'/players/(current|historic)/(c|pf|sf|sg|pg)', PlayersListHandler),
+        (r'/overview', tornado.web.RedirectHandler, {'url': '/overview/2014'}),
+        (r'/players/current', tornado.web.RedirectHandler, {'url': '/players/current/c'}),
+        (r'/players/historic', tornado.web.RedirectHandler, {'url': '/players/historic/c'}),
+        (r'/players/info', PlayersOverviewHandler),
     ], **settings)
 
 def on_shutdown():
