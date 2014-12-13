4f3d36054b2ca02fcef10b1f0b28542deba91596
LiuYi <yl3068@nyu.edu>
2014-12-13 17:47:52 -0500

    format templates, add notes on plots

--- webapp/nbastats/plotting.py
+++ webapp/nbastats/plotting.py
@@ -15,12 +15,13 @@ def hist_plot(data):
 
 def pie_plot(data):
     """ plot pie chart and return html code """
-    fig = plt.figure(figsize=(5, 5))
+    fig = plt.figure(figsize=(4.5, 4.5))
     ax = fig.add_subplot(111, axisbg='#FFFFFF')
     labels = 'C', 'PF', 'SF', 'SG', 'PG'
     colors = ['skyblue', 'yellowgreen', 'lightcoral', 'mediumpurple', 'gold']
     explode = (data==max(data)) * 0.1
     ax.pie(data, labels=labels, colors = colors, startangle=90, autopct='%1.1f%%', explode=explode)
+    plt.tight_layout()
     html = mpld3.fig_to_html(fig)
     plt.close()
     return html
@@ -31,12 +32,13 @@ def trend_plot(data):
     counts = np.array([x[1] for x in sorted(data.items(), key=lambda x:x[0])])
     labels = 'C', 'PF', 'SF', 'SG', 'PG'
     colors = ['skyblue', 'yellowgreen', 'lightcoral', 'mediumpurple', 'gold']
-    fig = plt.figure(figsize=(7, 5))
+    fig = plt.figure(figsize=(6.5, 4.5))
     ax = fig.add_subplot(111, axisbg='#EEEEEE')
     ax.grid(color='white', linestyle='solid')
     for i in xrange(5):
         ax.plot(years, counts[:, i], label=labels[i], color=colors[i], linewidth=2.0)
     ax.legend(loc=4)
+    plt.tight_layout()
     html = mpld3.fig_to_html(fig)
     plt.close()
     return html
